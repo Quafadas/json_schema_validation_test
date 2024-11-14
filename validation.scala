c73e1577ed10bd2099a4ea37ec82c80a29354337
Simon Parten <quafadas@gmail.com>
2024-11-14 20:56:34 +0100

    nope

--- validation.scala
+++ validation.scala
@@ -11,12 +11,15 @@ import upickle.core.Visitor
 @main def validate =
 
   val schema = scala.io.Source.fromFile("schema.json").toString()
+  val registry = new MutableRegistry()
 
   val sch: Schema = json_schema.from(
     ujson.Readable,
-    ujson.Readable.fromString(schema)
+    ujson.Readable.fromString(schema),
+    registry = registry
   )
-  val validator: Visitor[?, OutputUnit] = json_schema.validator(sch)
+  val validator: Visitor[?, OutputUnit] =
+    json_schema.validator(sch, registry = registry)
   val result: OutputUnit = ujson.Str("foo").transform(validator)
 
   println(result.valid)
