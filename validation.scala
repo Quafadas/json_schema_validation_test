6a12a45217398543c9e07d5ee79cfc39b0eed153
Simon Parten <quafadas@gmail.com>
2024-11-14 19:06:21 +0100

    try suggestion

--- validation.scala
+++ validation.scala
@@ -2,18 +2,21 @@
 //> using dep com.lihaoyi::upickle::4.0.2
 //> using dep io.github.jam01::json-schema::0.1.0
 //> using dep com.lihaoyi::requests::0.9.0
+
 import upickle.default._
 import io.github.jam01.json_schema.*
 import io.github.jam01.json_schema
 import upickle.core.Visitor
 
 @main def validate =
+  val registry = new MutableRegistry()
   val schema = requests.get(
     "https://github.com/vega/schema/raw/refs/heads/master/vega-lite/v5.21.0.json"
   )
   val sch: Schema = json_schema.from(
     ujson.Readable,
-    ujson.Readable.fromString(schema.text())
+    ujson.Readable.fromString(schema.text()),
+    registry = registry
   )
   val validator: Visitor[?, OutputUnit] = json_schema.validator(sch)
   val result: OutputUnit = ujson.Str("foo").transform(validator)
